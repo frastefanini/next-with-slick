54439743ab95f9617a3dda0878b3baa4b8d10c4f
Francesco Stefanini <francesco.stefanini@dbridge.info>
2024-02-19 15:27:15 +0100

    Add React Slick

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,6 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
   return null;
